edff2d0a2f02098b7bfc7ed369020ac19efd4a5c
kameliaT <kameliatakka12@gmail.com>
2026-04-03 17:14:02 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /**
  * @file main.c
  * @brief Interface utilisateur interactive du simulateur d'ordonnancement
- * @authors MISSAOUI Alissa (40%), TAKKA Kamelia (30%), HORNUNG Thomas (30%)
+ * @authors MISSAOUI Alissa (35%), TAKKA Kamelia (40%), HORNUNG Thomas (25%)
  * @date 2026-04-02
  * 
  * Ce fichier contient le menu principal, la gestion des entrees utilisateur,
